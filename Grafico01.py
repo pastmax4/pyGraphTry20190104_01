15b586df9ca93c57b1640f7638d8dee1872d3292
pastmax4 <46377444+pastmax4@users.noreply.github.com>
2019-01-04 16:01:40 +0100

    Add files via upload

---
+++ Grafico01.py
@@ -0,0 +1,16 @@
+'''
+Created on 13 dic 2017
+
+@author: mpasteri
+'''
+import pylab
+import numpy
+
+x = numpy.linspace(-15,15,100) # 100 linearly spaced numbers
+y = numpy.sin(x)/x # computing the values of sin(x)/x
+
+# compose plot
+pylab.plot(x,y) # sin(x)/x
+pylab.plot(x,y,'co') # same function with cyan dots
+pylab.plot(x,2*y,x,3*y,x,10*y) # 2*sin(x)/x and 3*sin(x)/x
+pylab.show() # show the plot
